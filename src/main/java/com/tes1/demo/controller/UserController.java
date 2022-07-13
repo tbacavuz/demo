1f11f451e329649deffe5631babea8a674b0a1fa
Tolga Bacavuz <tolgabacavuz@gmail.com>
2022-07-13 12:57:14 +0100

    Moved logic to services package

--- src/main/java/com/tes1/demo/controller/UserController.java
+++ src/main/java/com/tes1/demo/controller/UserController.java
@@ -1,31 +1,18 @@
 package com.tes1.demo.controller;
 
 import com.tes1.demo.dto.UserDetailsDTO;
+import com.tes1.demo.service.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    public static String MyName = "Tolga";
-    public static String MyLocation = "Portugal";
-    public static String MyCompany = "Teleperformance";
+    @Autowired private UserServices userServices;
 
     @PostMapping(value = "/user")
     public String user(@RequestBody UserDetailsDTO userDetailsDTO) {
-        if (userDetailsDTO.getName().equals(MyName)
-                && userDetailsDTO.getLocation().equals(MyLocation)
-                && userDetailsDTO.getCompany().equals(MyCompany)) {
-            return "Merhaba, I am "
-                    + userDetailsDTO.getName()
-                    + " and this is my first SpringBoot Project!";
-        }
-        //using else here was redundant
-        return "Hi, I am "
-                + userDetailsDTO.getName()
-                + " from "
-                + userDetailsDTO.getCompany()
-                + ", "
-                + userDetailsDTO.getLocation();
+        return userServices.getWelcomeMessage(userDetailsDTO);
     }
 }
