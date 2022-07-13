c36fcc9f87bda993bfd2616859b415fb4d0af913
Tolga Bacavuz <tolgabacavuz@gmail.com>
2022-07-13 12:12:04 +0100

    Special message if the user is the Author. (if-else condition)

--- src/main/java/com/tes1/demo/controller/UserController.java
+++ src/main/java/com/tes1/demo/controller/UserController.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    public static String MyName = "Tolga";
+    public static String MyLocation = "Portugal";
+    public static String MyCompany = "Teleperformance";
 
     @PostMapping(value = "/user")
     public String user(@RequestBody UserDetailsDTO userDetailsDTO) {
+        if (userDetailsDTO.getName().equals(MyName)
+                && userDetailsDTO.getLocation().equals(MyLocation)
+                && userDetailsDTO.getCompany().equals(MyCompany)) {
+            return "Merhaba, I am "
+                    + userDetailsDTO.getName()
+                    + " and this is my first SpringBoot Project!";
+        }
+        //using else here was redundant
         return "Hi, I am "
                 + userDetailsDTO.getName()
                 + " from "
